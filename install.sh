e55d9c3d9fc3938fa2c901213a7fe9e94df61c65
Josh Priestley <paystey2k5@gmail.com>
2014-07-10 14:36:12 +0100

    Added libssl to requirements for building

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get -y install make ruby1.9.1 ruby1.9.1-dev git-core \
-libpcre3-dev libxslt1-dev libgd2-xpm-dev libgeoip-dev unzip zip build-essential
+libpcre3-dev libxslt1-dev libgd2-xpm-dev libgeoip-dev unzip zip build-essential libssl-dev
 
 sudo gem install fpm
